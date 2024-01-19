7d0de2cf05d496c0b8997f203f1c56fa54a2fc8b
Asep Hikmat Fatahillah <asephikmatf@gmail.com>
2024-01-19 16:31:39 +0700

    try to load image from remote url

--- Learnimal/Module/Common/FeedCell.swift
+++ Learnimal/Module/Common/FeedCell.swift
@@ -33,15 +33,42 @@ struct FeedCell: View {
                 })
             }
             
-            Image(uiImage: UIImage())
-                .resizable()
-                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                .aspectRatio(contentMode: .fit)
-                .clipShape(RoundedRectangle(cornerRadius: 40))
-                .background(Color.gray)
-                .onTapGesture(count: 2) {
-                    isFavorite.toggle()
+            AsyncImage(url: URL(string: "https://gist.github.com/alienlebarge/605560c2961cb3025038/raw/b75d209134f0fb64f04a5ee9ff274ceabf6e26e8/dodgetocat_v2.png")) { phase in
+                switch phase {
+                case .empty:
+                    ZStack(alignment: .center) {
+                        Image(uiImage: UIImage())
+                            .resizable()
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(RoundedRectangle(cornerRadius: 40))
+                            .background(Color.gray)
+                            .onTapGesture(count: 2) {
+                                isFavorite.toggle()
+                            }
+                        
+                        ProgressView()
+                    }
+                case .success(let image):
+                    image.resizable()
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 40))
+                        .onTapGesture(count: 2) {
+                            isFavorite.toggle()
+                        }
+                default:
+                    Image(uiImage: UIImage())
+                        .resizable()
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 40))
+                        .background(Color.gray)
+                        .onTapGesture(count: 2) {
+                            isFavorite.toggle()
+                        }
                 }
+            }
             
             Text(model.title)
             
