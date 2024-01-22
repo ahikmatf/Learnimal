f57134932cd3b575a4f105a4ad22777b2ef103b7
Asep Hikmat Fatahillah <asephikmatf@gmail.com>
2024-01-22 21:18:47 +0700

    add loading when opening feed

--- Learnimal/Module/Detail/AnimalFeedView.swift
+++ Learnimal/Module/Detail/AnimalFeedView.swift
@@ -10,37 +10,46 @@ import SwiftUI
 struct AnimalFeedView: View {
     var name: String
     @StateObject var viewModel = AnimalFeedViewModel(store: PersistenceStore.shared)
+    @State var isLoading = true
     @State private var showTabBar = false
     @State private var isFavorite = false
     
     var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 0) {
-                ForEach(0..<viewModel.animalImages.count, id: \.self) { index in
-                    let animalImage = viewModel.animalImages[index]
-                    
-                    FeedCell(
-                        model: FeedModel(imageViewModel: animalImage),
-                        likeable: true,
-                        imageDidDoubleTap: {
-                            imageDidDoubleTap(isFavorite: !animalImage.isFavorite, model: animalImage)
-                        }
-                    )
+        ZStack {
+            ScrollView {
+                LazyVStack(spacing: 0) {
+                    ForEach(0..<viewModel.animalImages.count, id: \.self) { index in
+                        let animalImage = viewModel.animalImages[index]
+                        
+                        FeedCell(
+                            model: FeedModel(imageViewModel: animalImage),
+                            likeable: true,
+                            imageDidDoubleTap: {
+                                imageDidDoubleTap(isFavorite: !animalImage.isFavorite, model: animalImage)
+                            }
+                        )
+                    }
+                }
+                .padding()
+            }
+            .onAppear {
+                showTabBar = false
+                isLoading = true
+                viewModel.fetchAnimalImages(name: name) {
+                    viewModel.markFavoriteImages()
+                    isLoading = false
                 }
             }
-            .padding()
+            .onDisappear { showTabBar = true }
+            .toolbar(showTabBar ? .visible : .hidden, for: .tabBar)
+            .navigationTitle(name)
+            .navigationBarTitleDisplayMode(.inline)
+            .scrollIndicators(.hidden)
+            .opacity(isLoading ? 0 : 1)
+            
+            ProgressView()
+                .opacity(isLoading ? 1 : 0)
         }
-        .onAppear {
-            showTabBar = false
-            viewModel.fetchAnimalImages(name: name) {
-                viewModel.markFavoriteImages()
-            }
-        }
-        .onDisappear { showTabBar = true }
-        .toolbar(showTabBar ? .visible : .hidden, for: .tabBar)
-        .navigationTitle(name)
-        .navigationBarTitleDisplayMode(.inline)
-        .scrollIndicators(.hidden)
     }
     
     func imageDidDoubleTap(isFavorite: Bool, model: AnimalImageViewModel) {
